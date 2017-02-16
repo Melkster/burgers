6302dc002c368318b474463f5b6d59d5eadcc9cc
Melkster <melker.annat@gmail.com>
2017-02-16 14:39:06 +0000

    Started on the bar page

--- js/app.js
+++ js/app.js
@@ -3,7 +3,20 @@ var app = angular.module('app', []).
 	templateUrl: 'templates/bar.html',
 	controller: function($scope) {
 	  $scope.hej = 'Hej';
-	  $scope.title = 'I like trains.'
+	  $scope.title = 'I like trains.';
+	  $scope.getNames = function(burgers){
+		console.log("hej");
+		return burgers;
+	  };
 	}
   });
 
+//function getBurgerNames(burgers) {
+  //names = [];
+  //for b in burgers names.push(burgers[b].name);
+  //return names;
+//}
+
+var brgs = [
+  {'name': 'b1'}
+]
