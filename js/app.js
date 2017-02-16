428ee6fd7ac5bd001ae0b950960d5c1009c6261e
Melkster <melker.annat@gmail.com>
2017-02-16 16:41:41 +0000

    Started implementing 'order-status'

--- js/app.js
+++ js/app.js
@@ -3,8 +3,19 @@ var app = angular.module('app', []);
 app.component('bar', {
 	templateUrl: 'templates/bar.html',
 	controller: function($scope) {
-	  $scope.hej = 'Hej';
-	  $scope.title = 'I like trains.';
 	  $scope.burgers = burgers;
+	  $scope.order = [];
+	  $scope.addItem = function(item) {
+		var itemInList = $scope.itemExists($scope.order, item);
+		if (itemInList === null) $scope.order.push({ 'name': item, 'amount': 1 });
+		else {
+		  itemInList.amount++;
+		}
+	  };
+
+	  $scope.itemExists = function(list, item) {
+		for (i in list) if (list[i].item == item) return list[i];
+		return null;
+	  };
 	}
-  });
+});
