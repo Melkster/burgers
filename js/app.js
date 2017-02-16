871de93a4474eb7d257d95ab5acd33c1be942b15
Melkster <melker.annat@gmail.com>
2017-02-16 15:45:21 +0000

    Started styling burger bars

--- js/app.js
+++ js/app.js
@@ -1,14 +1,10 @@
-var app = angular.module('app', []).
-  component('bar', {
+var app = angular.module('app', []);
+
+app.component('bar', {
 	templateUrl: 'templates/bar.html',
 	controller: function($scope) {
 	  $scope.hej = 'Hej';
 	  $scope.title = 'I like trains.';
 	  $scope.burgers = burgers;
-	  $scope.getBurgerNames = function(burgers) {
-		names = [];
-		for (b in burgers) names.push(burgers[b].name);
-		return names;
-	  };
 	}
   });
