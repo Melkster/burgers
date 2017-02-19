b68e5b84113eee9abed21f2882704ba9a58fe473
Melkster <melker.annat@gmail.com>
2017-02-19 15:31:39 +0000

    First attempt at removing items from order. It currently removes all items of that perticular type

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ app.component('bar', {
 	controller: function($scope) {
 	  $scope.burgers = burgers;
 	  $scope.order = [];
+
 	  $scope.addItem = function(item) {
 		var itemInList = $scope.itemExists($scope.order, item);
 		var newItem = {
@@ -13,14 +14,25 @@ app.component('bar', {
 		}
 
 		if (itemInList === null) $scope.order.push(newItem);
-		else {
-		  itemInList.amount++;
-		}
+		else itemInList.amount++;
 	  };
+
+	  $scope.removeItem = function(item, items) {
+		for (i in items) if (angular.equals(items[i].item, item)) {
+		  items.splice(i, 1); //?
+		  return;
+		}
+	  }
 
 	  $scope.itemExists = function(list, item) {
 		for (i in list) if (angular.equals(list[i].item.name, item.name)) return list[i];
 		return null;
 	  };
+
+	  //$scope.getItem = function(itemName, items) {
+		//for (i in items) if (items[i].name == itemName) {
+		  //return items[i];
+		//}
+	  //}
 	}
 });
