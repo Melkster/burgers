8534d095a785c651e94f87b4c2145902f250ac73
Melkster <melker.annat@gmail.com>
2017-03-02 15:08:40 +0000

    Added 'customize' button

--- js/app.js
+++ js/app.js
@@ -64,5 +64,31 @@ app.component('bar', {
 		  //return items[i];
 		//}
 	  //}
+	  //$scope.showCustomize = function($event) {
+		//var parentEl = angular.element(document.body);
+		//$mdDialog.show({
+		  //parent: parentEl,
+		  //targetEvent: $event,
+		  //template:
+		  //'<md-dialog aria-label="List dialog">' +
+		  //'  <md-dialog-content>'+
+		  //'    <md-list>'+
+		  //'      <md-list-item ng-repeat="item in items">'+
+		  //'       <p>Number {{item}}</p>' +
+		  //'      '+
+		  //'    </md-list-item></md-list>'+
+		  //'  </md-dialog-content>' +
+		  //'  <md-dialog-actions>' +
+		  //'    <md-button ng-click="closeDialog()" class="md-primary">' +
+		  //'      Close Dialog' +
+		  //'    </md-button>' +
+		  //'  </md-dialog-actions>' +
+		  //'</md-dialog>',
+		  //locals: {
+			//items: $scope.items
+		  //},
+		  //controller: DialogController
+		//});
+	  //}
 	}
 });
